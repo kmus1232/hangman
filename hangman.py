418ad72d1636fbe7c3330a98a1f81e0732e2b2e9
kmus1232 <kmus1232@gmail.com>
2023-04-28 15:49:04 +0900

    modified greeting message

--- hangman.py
+++ hangman.py
@@ -132,7 +132,7 @@ def checkWrongAnswer(missedLetters, secretWord):
 
 def main():
     """Main application entry point."""
-    print('H A N G M A N by ...')
+    print('H A N G M A N by minseok')
     missedLetters = ''
     correctLetters = ''
     gameSucceeded = False
